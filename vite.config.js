373545e96507e1670c97a70c8d7388995e6c5620
amirsalahshur <125862754+amirsalahshur@users.noreply.github.com>
2025-08-03 11:48:28 +0400

    fixed vite.config.js
    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
 
 export default defineConfig(({ command, mode }) => {
   const isProduction = mode === 'production';
@@ -84,8 +86,8 @@ export default defineConfig(({ command, mode }) => {
       },
       postcss: {
         plugins: isProduction ? [
-          require('autoprefixer'),
-          require('cssnano')({
+          autoprefixer,
+          cssnano({
             preset: ['default', {
               discardComments: { removeAll: true },
               normalizeWhitespace: true
